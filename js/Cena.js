160f094fa47d3256784b4c916efa3d4300f19b74
DanielPortes <fagundes.daniel191@gmail.com>
2021-12-21 20:11:47 -0300

    cria e usa uma classe Cena

---
+++ js/Cena.js
@@ -0,0 +1,14 @@
+export default class Cena
+{
+    constructor(canvas)
+    {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+    }
+
+    desenhar()
+    {
+        this.fillStyle = "grey";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
