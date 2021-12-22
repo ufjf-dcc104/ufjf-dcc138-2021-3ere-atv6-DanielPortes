a4dd8fe9796513df345b363b74206ae687649255
DanielPortes <fagundes.daniel191@gmail.com>
2021-12-22 11:39:21 -0300

    Marca sprites colididos para remocao

--- js/Cena.js
+++ js/Cena.js
@@ -5,6 +5,7 @@ export default class Cena
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.sprites = [];
+        this.aRemover = [];
         this.t0 = 0;
         this.dt = 0;
         this.idAnim = null;
@@ -72,9 +73,22 @@ export default class Cena
                 const spriteB = this.sprites[b];
                 if (spriteA.colidiumCom(spriteB))
                 {
-                    console.log(spriteA, spriteB);
+                    this.quandoColidir(spriteA, spriteB);
                 }
             }
         }
+    }
+
+    quandoColidir(a, b)
+    {
+        if (!this.aRemover.includes(a))
+        {
+            this.aRemover.push(a);
+        }
+        if (!this.aRemover.includes(b))
+        {
+            this.aRemover.push(b);
+        }
+        console.log(this.aRemover);
     }
 }
