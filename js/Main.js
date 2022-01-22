407cab9616ab56d46f9fc7229c3ece1f08b3eba5
DanielPortes <fagundes.daniel191@gmail.com>
2022-01-22 11:26:39 -0300

    Cria gerenciador de entrada de controle

--- js/Main.js
+++ js/Main.js
@@ -4,10 +4,11 @@ import AssetManager from "./AssetManager.js";
 import Mixer from "./Mixer.js";
 import Mapa from "./Mapa.js";
 import modeloMapa1 from "../maps/mapa1.js";
+import InputManager from "./InputManager.js";
 
+const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
-// const assets = new AssetManager();
 
 assets.carregaImagem("garota", "assets/garota.png");
 assets.carregaImagem("esqueleto", "assets/skelly.png");
@@ -18,19 +19,28 @@ assets.carregaAudio("boom", "assets/boom.wav");
 const canvas = document.querySelector("canvas");
 canvas.width = 14 * 32;
 canvas.height = 10 * 32;
-const cena1 = new Cena(canvas,assets);
 
-const mapa1 = new Mapa(10,14,32);
+input.configurarTeclado(
+    {
+        "ArrowLeft": "MOVE_ESQUERDA",
+        "ArrowRight": "MOVE_DIREITA",
+    }
+);
+
+
+const cena1 = new Cena(canvas, assets);
+
+const mapa1 = new Mapa(10, 14, 32);
 mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
 
-const pc = new Sprite({x:50, vx:10});
-const en1 = new Sprite({x:160,vx:-10, color: "red"});
+const pc = new Sprite({x: 50, vx: 10});
+const en1 = new Sprite({x: 160, vx: -10, color: "red"});
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
-cena1.adicionar(new Sprite({x:115,y: 70, vy: 10, color: "red"}));
-cena1.adicionar(new Sprite({x:115,y: 160, vy: -10, color: "red"}));
+cena1.adicionar(new Sprite({x: 115, y: 70, vy: 10, color: "red"}));
+cena1.adicionar(new Sprite({x: 115, y: 160, vy: -10, color: "red"}));
 
 cena1.iniciar();
 
