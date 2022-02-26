0140af28aecf774903a3fee7d05c304058d98d48
Daniel <fagundes.daniel191@gmail.com>
2022-02-26 20:16:48 -0300

    inimigos rebatem e sao jogados para baixo

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,12 +1,15 @@
-export default class Sprite {
+export default class Sprite
+{
     constructor({
                     x = 100, y = 100,
                     w = 20, h = 20,
                     color = "white", vx = 0,
-                    vy = 0, controlar = () => {
+                    vy = 0, controlar = () =>
+        {
         }
                     , tags = []
-                } = {}) {
+                } = {})
+    {
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -19,52 +22,63 @@ export default class Sprite {
         this.my = 0;
         this.controlar = controlar
         this.tags = new Set();
-        tags.forEach((tag) => {
+        tags.forEach((tag) =>
+        {
             this.tags.add(tag);
         });
     }
 
-    controlar(dt) {
+    controlar(dt)
+    {
 
     }
 
-    desenhar(ctx, assets) {
-        if (this.tags.has("special")) {
+    desenhar(ctx, assets)
+    {
+        if (this.tags.has("special"))
+        {
             ctx.drawImage(assets.img("eye"), this.x, this.y, 32, 32);
-        } else if (this.tags.has("enemy")) {
+        } else if (this.tags.has("enemy"))
+        {
             ctx.drawImage(assets.img("skull"), this.x, this.y, 32, 32);
 
-        } else if (this.tags.has("pc")) {
+        } else if (this.tags.has("pc"))
+        {
             ctx.drawImage(assets.img("doguinho"), this.x, this.y, 32, 32);
 
-        } else {
+        } else
+        {
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
         }
     }
 
-    mover(dt) {
+    mover(dt)
+    {
         this.x = this.x + this.vx * dt;
         this.y = this.y + this.vy * dt;
         this.mx = Math.floor(this.x / this.cena.mapa.SIZE);
         this.my = Math.floor(this.y / this.cena.mapa.SIZE);
     }
 
-    passo(dt) {
+    passo(dt)
+    {
         this.controlar(dt);
         this.mover(dt);
     }
 
-    colidiuCom(outro) {
+    colidiuCom(outro)
+    {
         return !(
             (this.x - this.w / 2 > outro.x + outro.w / 2) ||
-            (this.x + this.w / 2 < outro.x - outro.w / 2) ||
+            (this.x - this.w / 2 > outro.x + outro.w / 2) ||
             (this.y - this.h / 2 > outro.y + outro.h / 2) ||
             (this.y + this.h / 2 < outro.y - outro.h / 2)
         );
     }
 
-    aplicaRestricoes(dt) {
+    aplicaRestricoes(dt)
+    {
         this.aplicaRestricoesCima(this.mx, this.my - 1);
         this.aplicaRestricoesCima(this.mx - 1, this.my - 1);
         this.aplicaRestricoesCima(this.mx + 1, this.my - 1);
@@ -79,10 +93,13 @@ export default class Sprite {
         this.aplicaRestricoesBaixo(this.mx, this.my + 1);
     }
 
-    aplicaRestricoesDireita(pmx, pmy) {
+    aplicaRestricoesDireita(pmx, pmy)
+    {
         const SIZE = this.cena.mapa.SIZE
-        if (this.vx > 0) {
-            if (this.cena.mapa.tiles[pmy][pmx] !== 0) {
+        if (this.vx > 0)
+        {
+            if (this.cena.mapa.tiles[pmy][pmx] !== 0)
+            {
                 const tile = {
                     x: pmx * SIZE + SIZE / 2,
                     y: pmy * SIZE + SIZE / 2,
@@ -91,11 +108,14 @@ export default class Sprite {
                 }
                 this.cena.ctx.strokeStyle = 'white'
                 this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE)
-                if (this.colidiuCom(tile)) {
-                    for (const sprite of this.cena.sprites) {
-                        if (sprite.tags.has("enemy")) {
-                            sprite.vx = -100;
-                            sprite.vy += 1;
+                if (this.colidiuCom(tile))
+                {
+                    for (const sprite of this.cena.sprites)
+                    {
+                        if (sprite.tags.has("enemy"))
+                        {
+                            sprite.vx = -sprite.vx;
+                            sprite.y += 15;
                         }
                     }
                     this.x = tile.x - tile.w / 2 - this.w / 2 - 1
@@ -105,10 +125,14 @@ export default class Sprite {
         }
     }
 
-    aplicaRestricoesEsquerda(pmx, pmy) {
+    aplicaRestricoesEsquerda(pmx, pmy)
+    {
+
         const SIZE = this.cena.mapa.SIZE
-        if (this.vx < 0) {
-            if (this.cena.mapa.tiles[pmy][pmx] !== 0) {
+        if (this.vx < 0)
+        {
+            if (this.cena.mapa.tiles[pmy][pmx] !== 0)
+            {
                 const tile = {
                     x: pmx * SIZE + SIZE / 2,
                     y: pmy * SIZE + SIZE / 2,
@@ -117,24 +141,31 @@ export default class Sprite {
                 }
                 this.cena.ctx.strokeStyle = 'white'
                 this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE)
-                if (this.colidiuCom(tile)) {
-                    for (const sprite of this.cena.sprites) {
-                        if (sprite.tags.has("enemy")) {
-                            sprite.vx = 100;
-                            sprite.vy += 1;
+                if (this.colidiuCom(tile))
+                {
+                    this.x = tile.x + tile.w / 2 + this.w / 2 + 1
+                    for (const sprite of this.cena.sprites)
+                    {
+                        if (sprite.tags.has("enemy"))
+                        {
+                            sprite.vx = -sprite.vx;
+                            sprite.y += 15;
                         }
                     }
-                    this.x = tile.x + tile.w / 2 + this.w / 2 + 1
                 }
             }
         }
     }
 
-    aplicaRestricoesBaixo(pmx, pmy) {
+    aplicaRestricoesBaixo(pmx, pmy)
+    {
+
         const SIZE = this.cena.mapa.SIZE
 
-        if (this.vy > 0) {
-            if (this.cena.mapa.tiles[pmy][pmx] !== 0) {
+        if (this.vy > 0)
+        {
+            if (this.cena.mapa.tiles[pmy][pmx] !== 0)
+            {
                 const tile = {
                     x: pmx * SIZE + SIZE / 2,
                     y: pmy * SIZE + SIZE / 2,
@@ -143,7 +174,8 @@ export default class Sprite {
                 }
                 this.cena.ctx.strokeStyle = 'white'
                 this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE)
-                if (this.colidiuCom(tile)) {
+                if (this.colidiuCom(tile))
+                {
                     this.vy = 0
                     this.y = tile.y - tile.h / 2 - this.h / 2 - 1
                 }
@@ -152,11 +184,15 @@ export default class Sprite {
         }
     }
 
-    aplicaRestricoesCima(pmx, pmy) {
+    aplicaRestricoesCima(pmx, pmy)
+    {
+
         const SIZE = this.cena.mapa.SIZE
 
-        if (this.vy < 0) {
-            if (this.cena.mapa.tiles[pmy][pmx] !== 0) {
+        if (this.vy < 0)
+        {
+            if (this.cena.mapa.tiles[pmy][pmx] !== 0)
+            {
                 const tile = {
                     x: pmx * SIZE + SIZE / 2,
                     y: (pmy * SIZE) + 32,
@@ -165,7 +201,8 @@ export default class Sprite {
                 }
                 this.cena.ctx.strokeStyle = 'white'
                 this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE)
-                if (this.colidiuCom(tile)) {
+                if (this.colidiuCom(tile))
+                {
                     this.vy = 0
                     this.y = tile.y + 32 + tile.h / 2 + this.h / 2 + 1
                 }
